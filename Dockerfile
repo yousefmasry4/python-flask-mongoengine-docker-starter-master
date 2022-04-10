9d42a98cb0cfc51094394858ea107bdade11c69a
youssef <youssefmasry04@gmail.com>
2022-04-10 02:49:08 +0200

    solve docker problems

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM python:3.8.2
+FROM python:3.9.7
 WORKDIR /code
 COPY requirements.txt ./
 RUN pip install -r requirements.txt
 COPY . .
 EXPOSE 5100
-#CMD [ "python", "api/app.py" ]
+CMD [ "python", "api/app.py" ]
--- api/v1/resources/routes.py
+++ api/v1/resources/routes.py
@@ -1,5 +1,10 @@
-from v1.resources.todos import todos
+# some_file.py
+import sys
+
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(0, '/code')
+from api.v1.resources.todos import todos
 
 
-def initialize_routes(api):
-    api.add_namespace(todos)
+def initialize_routes(apis):
+    apis.add_namespace(todos)
